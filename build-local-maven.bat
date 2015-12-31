dd86e1eae68efbb6e93c3fee3b7cffbdf39d7f51
Dimitry Ivanov <mail@dimitryivanov.ru>
2015-12-31 12:10:01 +0300

    Added `runtime` module with fatJar artifact

--- build-local-maven.bat
+++ build-local-maven.bat
@@ -4,3 +4,4 @@ call gradlew.bat cursor-mock:buildUpload --configure-on-demand
 call gradlew.bat stormparser:parser-runtime:buildUpload stormparser:parser-apt:buildUpload --configure-on-demand
 call gradlew.bat core:buildUpload --configure-on-demand
 call gradlew.bat rx:buildUpload --configure-on-demand
+call gradlew.bat runtime:buildUpload --configure-on-demand
